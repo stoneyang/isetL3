047bec230449728bae2fdac65c0b34215e4db834
Qiyuan Tian <fdqiyuantian@gmail.com>
2014-06-01 10:37:49 -0700

    trained L3 filters for fb and rendered some example images in simulation

--- fb/s_render_for_fb.m
+++ fb/s_render_for_fb.m
@@ -1,4 +1,21 @@
 clear, clc, close all
 
-rawim = loadraw('001_Sunny_f16.0.RAW');
-img = imread('Sunny.JPG');
+%%
+s_initISET
+
+%% Load scene
+scene = sceneFromFile('AsianWoman_1.mat','multispectral');
+
+%% Render images
+sz = sceneGet(scene, 'size');
+
+%% Load camera
+load('L3camera_fb.mat');
+
+%% Specify luminance levels
+luminances = [1, 80];
+
+for lum = luminances
+    srgbResult = cameraComputesrgb(camera, scene, lum, sz, [], [], 1);
+    imwrite(srgbResult, ['srgb_fb_lum_' num2str(lum) '.png']);
+end
--- fb/s_train_for_fb.m
+++ fb/s_train_for_fb.m
@@ -11,3 +11,7 @@ L3 = L3Train(L3);
 
 %% Setup L3 camera
 camera = L3CameraCreate(L3);
+
+%% Save data
+save('L3camera_fb', 'camera');
+save('L3_fb', 'L3');
